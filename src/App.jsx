e1fe03c37b5fc18e3ed2e0c7f48b3b7f233ccfd5
Dean Kerr <dean.kerr@zendesk.com>
2022-06-03 15:51:43 +1000

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
-import { Header } from './header';
+import './App.css'
+import { Header } from './header'
 
 function App() {
   return (
     <div className="App">
       <Header>shopping with friends :)</Header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
