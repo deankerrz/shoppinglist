a8b9c1368b995b21eaea095a8cfac9c0966b450f
Dean Kerr <dean.kerr@zendesk.com>
2022-06-03 16:22:04 +1000

    Move components own folder

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from './Header'
+import { Footer } from './Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header>shopping with friends :)</Header>
+      <main>
+        <ul>
+          <li>Bread</li>
+          <li>Eggs</li>
+          <li>Cheese</li>
+        </ul>
+      </main>
+      <Footer>made with love by dean</Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+  text-align: center;
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 1vmin);
+  color: white;
+`
